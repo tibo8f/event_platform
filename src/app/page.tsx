74a81ce7d552347f1c39de94694a59044af53e81
tibo8f <83464121+tibo8f@users.noreply.github.com>
2024-10-16 22:13:53 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl">Evently</h1>
+      <Button className="px-10">Hello</Button>
+    </main>
+  );
+}
